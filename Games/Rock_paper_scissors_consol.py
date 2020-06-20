ea0d7984bff5c0668ac5ad139f339c0760070c68
romanua911 <kuc911@gmail.com>
2020-06-20 15:03:51 +0300

    Second step try using some oop +_-

--- Games/Rock_paper_scissors_consol.py
+++ Games/Rock_paper_scissors_consol.py
@@ -1,5 +1,16 @@
 import random
 
+class Player:
+    def __init__(self, chooses, identification, win, history):
+        self.identification = identification
+        self.chooses = chooses
+        self.win = win
+        self.history = history
+
+    def choose(self,chooses,history):
+        self.history = history.append(self.chooses)
+        self.chooses = chooses
+
 
 def main():
     print('============================')
@@ -8,22 +19,26 @@ def main():
     print('|| Rock, paper, scissors  ||')
     print('============================')
     count = count_round()
+    print('============================')
+    print('|| Game start             ||')
+    print('============================')
+    game()
 
-def pve():
-    pass
+def game():
+    human = Player(0, 1, 0, [])
+    computer = Player(0, 1, 0, [])
 
-def pvp():
-    pass
+
+    return True
 
 def count_round():
-
     print('============================')
     print('||     How much round ?   ||')
     print('============================')
     while True:
         value = input('Value:')
         try:
-            count = int(value)
+            value = int(value)
         except ValueError:
             print('Valid number, please')
             continue
@@ -31,7 +46,8 @@ def count_round():
             break
         else:
             print("Valid range, please: 1-10")
-    return count
+    return value
 
-if __name__ == "__main__" :
+
+if __name__ == "__main__":
     main()
